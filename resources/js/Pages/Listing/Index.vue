0ca0df01bc77bee35472d81e6ca30bd4cc3d7eb6
Mehmet Emre <mehmetemrep@gmail.com>
2023-08-29 09:51:34 +0300

    added style to listing

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <Link :href="route('listing.show', listing.id)">
-      <ListingAddress :listing="listing" />>
-    </Link>
-    <Link :href="route('listing.edit', listing.id)">Edit</Link>
-    <Link :href="route('listing.destroy', listing.id)" method="DELETE" as="button">Delete</Link>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
+      <Link :href="route('listing.show', listing.id)">
+        <ListingAddress :listing="listing" />>
+      </Link>
+      <Link :href="route('listing.edit', listing.id)">Edit</Link>
+      <Link :href="route('listing.destroy', listing.id)" method="DELETE" as="button">Delete</Link>
+    </Box>
   </div>
 </template>
   
 <script setup>
 import { Link } from '@inertiajs/inertia-vue3'
-import ListingAddress from '../../Components/ListingAddress.vue'
+import ListingAddress from '@/Components/ListingAddress.vue'
+import Box from '@/Layouts/UI/Box.vue'
 
 defineProps({
   listings: Array,
