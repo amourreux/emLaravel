f518d240e0ec91b52f1728654dfb6a59bc8836cf
Mehmet Emre <mehmetemrep@gmail.com>
2023-08-30 09:27:13 +0300

    ref #1: added controller and routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,12 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show']);
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+  ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+  ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+  ->name('logout');
+
+  
